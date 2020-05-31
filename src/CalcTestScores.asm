f5d7e642fa0b0baa8f26ef84cb9f2c2e044a592e
Kevin Chavez <kevinchavez@rocketmail.com>
2020-05-31 13:49:53 -0700

    Initial Skeleton Setup

---
+++ src/CalcTestScores.asm
@@ -0,0 +1,13 @@
+; Program name: Test Score Calculator
+; A Team Last Minute program
+; Authors: Kevin Chavez, Julissa Mota, Jasmine Gaona, Brian Cabrera
+; Description: An LC-3 program that displays the minimum, maximum, and
+; 	       average grade of 5 test scores and displays the letter
+;	       grade associated with the test scores
+; Input: User is prompted to input the test scores via keyboard
+; Output: Displays maximum, minimum, average score and letter grade
+;	  equivalence (0 - 50 = F, 60 - 69 = D, 70 - 79 = C, 
+;	  80 - 89 = B, 90 - 100 = A) on the console
+
+; MAIN ROUTINE
+.ORIG x3000
